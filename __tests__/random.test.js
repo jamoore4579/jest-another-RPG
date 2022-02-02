ce77f893265149220778a5a713cef729b91cac7e
Irish Moore <jasonamoore@frontier.com>
2022-02-01 22:50:20 -0500

    completed the potion code

--- __tests__/random.test.js
+++
@@ -1,7 +0,0 @@
-const { TestWatcher } = require('jest');
-const randomNumber = require('../lib/random.js');
-
-test('gets random number between 1 to 10', () => {
-    expect(randomNumber()).toBeGreaterThanOrEqual(1);
-    expect(randomNumber()).toBeLessThanOrEqual(10);
-});
